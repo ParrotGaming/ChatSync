5b1e5cb4674f43c0523a3144ea75ae3a2e42d9ee
ParrotGaming <killian@onyxwear.com>
2022-04-22 17:14:35 -0700

    updated for 1.18.2+

--- Commands.java
+++ Commands.java
@@ -5,10 +5,12 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class Commands extends ListenerAdapter{
-	public void onGuildMessageReceived(MessageReceivedEvent event) {
+	
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
 		String message = event.getMessage().getContentStripped();
 		
-		if(event.getMessage().getAuthor() != Main.jda.getSelfUser()) {
+		if(event.getMessage().getAuthor() != Main.jda.getSelfUser()) {	
 			String[] readerArgs = Main.cacheFileArgs.split(" ");
 			if (event.getChannel().getName().equals(readerArgs[2])) {
 				Bukkit.broadcastMessage("(Discord) <" + event.getMessage().getAuthor().getName() + ">" + " " + message);	
